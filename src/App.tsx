37ce78997893bd1aabdedea69f5e8b6b0a6f7553
Ivan Zotov <zia14leg@gmail.com>
2023-12-24 20:20:40 +0400

    feat: make draft layout for lists and results

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ import { findAllCountriesByLang } from "./utils";
 function App() {
   const [countries, setCountries] = useState<Country[] | null>(null);
   const [languages, setLanguages] = useState<Language[] | null>(null);
+  const [currentLang, setCurrentLang] = useState<Language | null>(null);
   const [countriesWithCurrentLang, setCountriesWithCurrentLang] = useState<
     Country[]
   >([]);
@@ -26,52 +27,56 @@ function App() {
     });
   }, []);
 
+  const langBtnsTitle = `Total languages quantity: ${languages?.length}`;
+
   const langBtns = languages
     ? languages.map((language) => {
         const { code, name } = language;
         return customButton({
           key: code,
           label: `${name} - ${code}`,
-          onClick: () =>
+          onClick: () => {
+            setCurrentLang(language);
             setCountriesWithCurrentLang(
               findAllCountriesByLang({ countries, currentLangCode: code })
-            ),
+            );
+          },
         });
       })
     : null;
 
+  const resultTitle = currentLang
+    ? `There ${countriesWithCurrentLang.length > 1 ? "are" : "is"} ${
+        countriesWithCurrentLang.length
+      } ${
+        countriesWithCurrentLang.length > 1 ? "countries" : "country"
+      } in the World, speaking on ${currentLang.name} language: `
+    : "Choose a language";
+
+  const result =
+    countriesWithCurrentLang &&
+    countriesWithCurrentLang.map((country, index) => {
+      return (
+        <div key={country.code}>
+          {index + 1}
+          {" - "}
+          {country.code}
+          {" - "}
+          {country.name} <span className={styles["flag"]}>{country.emoji}</span>
+        </div>
+      );
+    });
+
   return (
     <div className={styles["app-container"]}>
-      <div>
-        <span>Countries with chosen language: </span>
-        {countriesWithCurrentLang &&
-          countriesWithCurrentLang.map((country, index) => {
-            return (
-              <div key={country.code}>
-                {index + 1}
-                {" - "}
-                {country.code}
-                {" - "}
-                {country.name}{" "}
-                <span className={styles["flag"]}>{country.emoji}</span>
-              </div>
-            );
-          })}
+      <div className={styles["langs-container"]}>
+        <span>{langBtnsTitle}</span>
+        <div className={styles["langs-list"]}>{langBtns}</div>
       </div>
-      {langBtns}
-      {/* <ul>
-        {countries &&
-          countries.map((country) => {
-            return (
-              <li key={country.code}>
-                {country.code}
-                {" - "}
-                {country.name}{" "}
-                <span className={styles["flag"]}>{country.emoji}</span>
-              </li>
-            );
-          })}
-      </ul> */}
+      <div className={styles["results-container"]}>
+        <span>{resultTitle}</span>
+        <div className={styles["results-list"]}>{result}</div>
+      </div>
     </div>
   );
 }
